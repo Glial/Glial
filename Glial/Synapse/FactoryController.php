2d0c2c3553ed7e329a36b6fee590a16991bc7686
Esysteme <aurelien.lequoy@gmail.com>
2014-02-14 22:22:07 +0100

    Update FactoryController.php

--- Glial/Synapse/FactoryController.php
+++ Glial/Synapse/FactoryController.php
@@ -11,10 +11,13 @@ class FactoryController
 	/**
 	
 	 * (Glial 2.1)<br/>
-     	* Add a MVC node in a view 
+     	 * Add a MVC node in a view 
 	 * @author Aurélien LEQUOY, <aurelien.lequoy@esysteme.com>
 	 * @param string construct of controller
 	 * @return boolean Success
+	 * @package Controller
+	 * @since 2.1 First time this was introduced.
+	 * @description create a new MVC and display the output in standard flux
 	 * @access public
 	 */
 	public static function addNode($controller, $action, $param)
@@ -27,12 +30,16 @@ class FactoryController
 	/**
 	 * Short description of method okh
 	 *
-	 * @author Aurélien LEQUOY, <aurelien.lequoy@esysteme.com>
+	 * @author Aurélien LEQUOY <aurelien.lequoy@esysteme.com>
 	 * @license GPL
-	 * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+	 * @license http://opensource.org/licenses/GPL-3.0 GNU Public License
 	 * @param string construct of controller
 	 * @return boolean Success
+	 * @description should be called 1 time by request. It create the root MVC used in boot and display the output in standard flux. it's this controller witch will determine the layout.
 	 * @access public
+	 * @example \Glial\Synapse\FactoryController::rootNode("class", "function", array('param1','param2'));
+	 * @package Controller
+	 * @See Also addNode
 	 * @since 2.1 First time this was introduced.
 	 * @version 2.1
 	 */
