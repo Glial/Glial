108eaf4a5a46adf6083bb8529eff63b7114a0d96
Esysteme <aurelien.lequoy@gmail.com>
2014-02-15 11:34:18 +0100

    Update Recursive.php

--- Glial/Tools/Recursive.php
+++ Glial/Tools/Recursive.php
@@ -43,5 +43,23 @@ class Recursive
         }
         closedir($dh);
     }
+    
+/**
+ * Remove the directory and its content (all files and subdirectories).
+ * @param string $dir the directory name
+ * @author wang yun
+ */
+function rmrf($dir) {
+    foreach (glob($dir) as $file) {
+        if (is_dir($file)) { 
+            rmrf("$file/*");
+            rmdir($file);
+        } else {
+            unlink($file);
+        }
+    }
+}
+
+
 
 }
