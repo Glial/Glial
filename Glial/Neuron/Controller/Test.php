ac875c31935488d79f4720efe32b09af3c5d64d3
Esysteme <aurelien.lequoy@gmail.com>
2014-04-29 13:36:43 +0000

    add trait

---
+++ Glial/Neuron/Controller/Test.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace Glial\Neuron\Controller;
+
+trait Test {
+
+    function win() {
+        $this->view = false;
+        $win = new Window('Test windows', "SFRHSFGH x xfhg xfgh xfgh xgfh gxfh xfxgfh x xfgh xfgh xfgh xfgh : failed to open stream: No such file or directory in \n"
+                . "ok test msg :\n"
+                . "\n"
+                . "[[INPUT]]"
+                . "\n");
+    }
+
+}
