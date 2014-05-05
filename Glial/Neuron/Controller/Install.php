390b2fee8eb930b379803018b83e8ab9713e5586
Esysteme <aurelien.lequoy@gmail.com>
2014-05-05 09:18:09 +0000

    new install

--- Glial/Neuron/Controller/Install.php
+++ Glial/Neuron/Controller/Install.php
@@ -6,136 +6,42 @@ use \Glial\Cli\Glial;
 use \Glial\Cli\Color;
 
 trait Install {
-
     function index() {
+        //remove view 
         $this->view = false;
 
-//for display like putty in utf8
-        //shell_exec('echo -ne \'\\\\e%G\\\\e[?47h\\\\e%G\\\\e[?47l\\\'');
-
+        
+        //header
         echo PHP_EOL . Glial::header() . PHP_EOL;
 
-        echo $this->out("Installation in progress ...", "OK");
 
-
-        $res = shell_exec("git --version");
-        if (!preg_match("/^git version [1]\.[0-9]+\.[0-9]/i", $res, $gg)) {
-            $errors['git'] = true;
-        }
-
-        if (version_compare(PHP_VERSION, '5.5.10', '<')) {
-            $errors['php'] = PHP_VERSION;
-        }
-
-        if (!extension_loaded('gd')) {
-            $errors['gd'] = true;
-        }
-
-        if (!extension_loaded('mysqli')) {
-            $errors['mysqli'] = true;
-        }
-
-        if (!extension_loaded('curl')) {
-            $errors['curl'] = true;
-        }
-
-        if (!extension_loaded('ssh2')) {
-            $errors['ssh2'] = true;
-        }
-
-
-        if (!empty($errors)) {
-            echo $this->out("Check dependencies ...", "KO");
-
-            echo Color::getColoredString("Some settings on your machine make Glial unable to work properly.", "red") . PHP_EOL;
-            echo Color::getColoredString("Make sure that you fix the issues listed below and run this script again:", "red") . PHP_EOL;
-
-
-
-
-            foreach ($errors as $error => $current) {
-
-                $displayIniMessage = false;
-
-                switch ($error) {
-                    case 'gd':
-                        $text = PHP_EOL . "The gd extension is missing." . PHP_EOL;
-                        $text .= "Install it (\# apt-get install php5-gd) or recompile php without --disable-gd";
-                        $displayIniMessage = true;
-                        break;
-
-                    case 'git':
-                        $text = PHP_EOL . "The git software is missing." . PHP_EOL;
-                        $text .= "Install it (\# apt-get install git)";
-                        $displayIniMessage = true;
-                        break;
-
-                    case 'mysqli':
-                        $text = PHP_EOL . "The mysqli extension is missing." . PHP_EOL;
-                        $text .= "Install it or recompile php without --disable-mysqli";
-                        $displayIniMessage = true;
-                        break;
-
-
-                    case 'curl':
-                        $text = PHP_EOL . "The curl extension is missing." . PHP_EOL;
-                        $text .= "Install it or recompile php without --disable-curl";
-                        $displayIniMessage = true;
-                        break;
-
-                    case 'phar':
-                        $text = PHP_EOL . "The phar extension is missing." . PHP_EOL;
-                        $text .= "Install it or recompile php without --disable-phar";
-                        $displayIniMessage = true;
-                        break;
-
-                    case 'unicode':
-                        $text = PHP_EOL . "The detect_unicode setting must be disabled." . PHP_EOL;
-                        $text .= "Add the following to the end of your `php.ini`:" . PHP_EOL;
-                        $text .= "    detect_unicode = Off";
-                        $displayIniMessage = true;
-                        break;
-
-                    case 'suhosin':
-                        $text = PHP_EOL . "The suhosin.executor.include.whitelist setting is incorrect." . PHP_EOL;
-                        $text .= "Add the following to the end of your `php.ini` or suhosin.ini (Example path [for Debian]: /etc/php5/cli/conf.d/suhosin.ini):" . PHP_EOL;
-                        $text .= "    suhosin.executor.include.whitelist = phar " . $current;
-                        $displayIniMessage = true;
-                        break;
-
-                    case 'php':
-                        $text = PHP_EOL . "Your PHP ({$current}) is too old, you must upgrade to PHP 5.5.10 or higher.";
-                        $displayIniMessage = true;
-                        break;
-
-                    case 'allow_url_fopen':
-                        $text = PHP_EOL . "The allow_url_fopen setting is incorrect." . PHP_EOL;
-                        $text .= "Add the following to the end of your `php.ini`:" . PHP_EOL;
-                        $text .= "    allow_url_fopen = On";
-                        $displayIniMessage = true;
-                        break;
-
-                    case 'ioncube':
-                        $text = PHP_EOL . "Your ionCube Loader extension ($current) is incompatible with Phar files." . PHP_EOL;
-                        $text .= "Upgrade to ionCube 4.0.9 or higher or remove this line (path may be different) from your `php.ini` to disable it:" . PHP_EOL;
-                        $text .= "    zend_extension = /usr/lib/php5/20090626+lfs/ioncube_loader_lin_5.3.so";
-                        $displayIniMessage = true;
-                        break;
-                }
-                if ($displayIniMessage) {
-//$text .= $iniMessage;
-                    echo Color::getColoredString($text, "yellow") . PHP_EOL;
-                }
+        // test php version
+        $fct = function($msg) {
+            $err = "";
+            $version = '5.5.10';
+            if (version_compare(PHP_VERSION, $version, '<')) {
+                $err = "(Should be highter than : ".$version.")";
             }
 
-            $this->onError();
-        } else {
-            echo $this->out("Check dependencies ", "OK");
+            return array(true, $msg);
+        };
+        $this->anonymous($fct, "Check PHP version : ".PHP_VERSION." " .$err );
+
+
+        //test all extention php required
+        $extentions = array('gd', 'mysqli', 'curl', 'ssh2', 'phar');
+
+        foreach ($extentions as $ext) {
+
+            $fct = function($msg) {
+                
+                return array(extension_loaded($ext), $msg);
+            };
+            $this->anonymous($fct, "PHP version : " . $ext);
         }
 
 
-//making tree directory
-
+        //making tree directory
         $fct = function($msg) {
             $dirs = array("data", "data/img", "documentation", "tmp/crop", "tmp/documentation", "application/webroot/js",
                 "application/webroot/css", "application/webroot/file", "application/webroot/video", "application/webroot/image");
@@ -158,10 +64,8 @@ trait Install {
 
 
 
-
+        // replace and install lastest jQuery
         $fct = function ($msg) {
-
-
             $name = "jquery-latest.min.js";
             $jQuery = $_SERVER['PWD'] . "/application/webroot/js/" . $name;
 
@@ -189,8 +93,6 @@ trait Install {
                 return array(false, $msg);
             }
         };
-
-
         $this->anonymous($fct, "jQuery installed (%s)");
 
 
@@ -257,8 +159,6 @@ trait Install {
         echo $this->di['acl'];
         echo $this->di['db'];
 
-        //$this->di['db']->mainMenu();
-        //$file = Shell::prompt2('Please choose your answer or press Enter to continue: ', array('', '1', '2', '3')); 
     }
 
     function composer() {
