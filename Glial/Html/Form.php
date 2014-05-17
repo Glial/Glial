bc9666e057f86db8d28a561d12692cc9d5c27702
Esysteme <aurelien.lequoy@gmail.com>
2014-05-17 14:54:08 +0200

    Create Form.php

---
+++ Glial/Html/Form.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Glial\Html;
+
+
+class Form
+{
+
+  static public function input($table, $field, $options, $indice)
+  {
+  
+  
+    if ($indice != -1) {
+        if (!empty($_GET[$table][$indice][$field])) {
+            $value = $_GET[$table][$indice][$field];
+        } else {
+            $value = "";
+        }
+    } else {
+        if (!empty($_GET[$table][$field])) {
+            $value = $_GET[$table][$field];
+        } else {
+            $value = "";
+        }
+    }
+
+    if (!empty($_SESSION['ERROR'][$table][$field])) {
+        $error = " <span class=\"error\">" . $_SESSION['ERROR'][$table][$field] . "</span>";
+        $class = " error";
+
+        unset($_SESSION['ERROR'][$table][$field]);
+    } else {
+        $error = "";
+        $class = "";
+    }
+
+    if (!empty($classo))
+        $classo = "$classo ";
+
+
+    $extra="";
+    foreach($options as $key => $val)
+    {
+      $extra .= " ".$key."=".$val." ";
+    }
+
+
+    if ($indice != -1) {
+        return "<input id=\"" . $table . "-" . $indice . "-" . $field . "\" class=\"" . $classo . "text" . $class . "\" type=\"text\" name=\"" . $table . "[" . $indice . "][" . $field . "]\" value=\"" . $value . "\" />" . $error;
+    } else {
+        return "<input id=\"" . $table . "-" . $field . "\" class=\"" . $classo . $class . "\" type=\"text\" name=\"" . $table . "[" . $field . "]\" value=\"" . $value . "\" />" . $error;
+    }
+  
+  
+  }
+
+
+
+
+
+}
